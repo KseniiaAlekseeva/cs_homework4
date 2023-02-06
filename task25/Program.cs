96b3922478e0fb2eff5d85ab109feed0e067bf33
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-06 22:08:38 +0300

    Add task25

---
+++ task25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Raise number A to power B
+double a = 0.0;
+int b = 0;
+
+Console.WriteLine("Enter number A: ");
+a = double.Parse(Console.ReadLine());
+
+while (b < 1)
+{
+    Console.WriteLine("Enter an integer number B: ");
+    b = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"A^B = {a}^{b} = {myPow(a, b)}");
+
+double myPow(double a, int b)
+{
+    double res = 1;
+    for (int i = 0; i < b; i++) res *= a;
+    return res;
+}
