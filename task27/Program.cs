88879ec6981ed6f6384a80766f5601f3f289e12b
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-06 22:23:20 +0300

    Add task27

---
+++ task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Function calculates the sum of digits of the number
+Console.WriteLine("Enter an integer number: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The sum of digits of number {num} is {sumDigits(num)}");
+
+int sumDigits(int num)
+{
+    int sum = 0;
+    int curNum = Math.Abs(num);
+    while (curNum > 0)
+    {
+        sum += curNum % 10;
+        curNum = curNum / 10;
+    }
+
+    return sum;
+}
